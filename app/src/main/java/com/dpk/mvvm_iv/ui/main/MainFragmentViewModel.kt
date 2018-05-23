72f88740ca429c83440f42af0900eabc148e590f
曳光剂 <1242067286@qq.com>
2018-05-23 10:14:48 +0800

    05-23

--- app/src/main/java/com/dpk/mvvm_iv/ui/main/MainFragmentViewModel.kt
+++ app/src/main/java/com/dpk/mvvm_iv/ui/main/MainFragmentViewModel.kt
@@ -1,11 +1,19 @@
 package com.dpk.mvvm_iv.ui.main
 
 import android.databinding.BaseObservable
+import android.databinding.Bindable
 import android.databinding.ObservableBoolean
-import android.databinding.ObservableField
+import com.android.databinding.library.baseAdapters.BR
 
 class MainFragmentViewModel : BaseObservable() {
-    var listener = ObservableField<String>("")
-    var loading=ObservableBoolean(false)
 
+    var listener = ""
+        @Bindable get
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.listener)
+        }
+    val key = ObservableBoolean(false)
+    val loading = ObservableBoolean(false)
+    val issuccess = ObservableBoolean(false)
 }
